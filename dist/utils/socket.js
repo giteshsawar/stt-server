3ea79e467def7db318f4182e647bc872c1b1e908
vahe224 <vahe.hayrapetyan@softconstruct.com>
2021-11-01 17:47:50 +0400

    Fix socket streaming

--- dist/utils/socket.js
+++ dist/utils/socket.js
@@ -1,7 +1,75 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const socket_io_1 = require("socket.io");
-module.exports = function () {
-    const io = new socket_io_1.Server(server);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+const speech_1 = __importDefault(require("./speech"));
+const socket_io_stream_1 = __importDefault(require("socket.io-stream"));
+// const { pipeline, Readable } = require('stream');
+// import { Buffer } from 'buffer';
+// const path = require("path");
+// const fs = require("fs");
+const params = {
+    encoding: 'WEBM_OPUS',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US'
+    // streamingLimit: 290000
+};
+const config = {
+    encoding: params.encoding,
+    sampleRateHertz: params.sampleRateHertz,
+    languageCode: params.languageCode,
+};
+const request = {
+    config,
+    interimResults: false,
+};
+let recognizeStream;
+module.exports = function (io) {
+    io.on("connection", (socket) => {
+        socket.on("ping", () => {
+            console.log("socket connection pinged!!!!!!!");
+        });
+        (0, socket_io_stream_1.default)(socket).on("stream-transcribe", (stream, data) => {
+            transcribeAudioStream(stream, function (data) {
+                console.log("got results", data.results);
+                if (data && data.results[0] && data.results[0].alternatives[0]) {
+                    console.log("transcript data", data.results[0].alternatives[0].transcript);
+                }
+                if (data.results[0] && data.results[0].isFinal) {
+                    stopRecognitionStream();
+                    // console.log('restarted stream serverside');
+                }
+                socket.emit("results", data);
+            });
+        });
+    });
+};
+function stopRecognitionStream() {
+    if (recognizeStream) {
+        recognizeStream.end();
+        recognizeStream = undefined;
+    }
+}
+function transcribeAudioStream(audio, cb) {
+    console.log("recogninze strem", audio);
+    recognizeStream = speech_1.default.streamingRecognize(request)
+        .on('data', function (data) {
+        console.log(data);
+        cb(data);
+    })
+        .on('error', (e) => {
+        console.log(e);
+    })
+        .on('end', () => {
+        console.log('transcript end');
+        stopRecognitionStream();
+    });
+    audio.pipe(recognizeStream);
+    audio.on('end', function () {
+        console.log('audio end');
+        //fileWriter.end();
+    });
+}
+;
 //# sourceMappingURL=socket.js.map
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -33,32 +33,17 @@ module.exports = function(io) {
         });
 
         ss(socket).on("stream-transcribe", (stream, data) => {
-            // console.log("socket connection pinged!!!!!!!", stream);
-            // const filename = path.basename(data.name);
-            // stream.pipe(fs.createWriteStream(filename));
-            // socket.emit("data_received", {});
-
-            transcribeAudioStream(stream, function(data){
-                // console.log("got results", results);
-                if(data && data.results[0] && data.results[0].alternatives[0]) {
+            transcribeAudioStream(stream, function(data) {
+                console.log("got results", data.results);
+                if (data && data.results[0] && data.results[0].alternatives[0]) {
                     console.log("transcript data", data.results[0].alternatives[0].transcript);
                 }
                 if (data.results[0] && data.results[0].isFinal) {
                     stopRecognitionStream();
                     // console.log('restarted stream serverside');
                 }
-                socket.emit('results', data);
+                socket.emit("results", data);
             });
-
-            // const dataBuffer = Buffer.from(stream, 'base64');
-            // const fileStream = fs.createWriteStream('finalaudio.wav', {flags: 'a'});
-            // fileStream.write(dataBuffer);
-
-            // const readStream = fs.createReadStream(dataBuffer).pipe(recognizeStream);
-
-            // recognizeStream.write(fileStream);
-            // stream.pipe(recognizeStream);
-            // pipeline(readableStream, recognizeStream);
         });
       });
 }
@@ -71,7 +56,6 @@ function stopRecognitionStream() {
 }
 
 function transcribeAudioStream(audio, cb) {
-    // console.log("recogninze strem", recognizeStream);
     recognizeStream = speechClient.streamingRecognize(request)
     .on('data', function(data){
         console.log(data);
